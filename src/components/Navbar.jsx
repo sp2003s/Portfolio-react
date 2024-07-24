cc5e068bcc39ce8c68e2e06c25afedeaf1077e48
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-07-25 02:19:29 +0530

    navbar and hero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import {FaInstagram} from "react-icons/fa";
 const Navbar = () => {
     return <nav className = "mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 item-center">
-            <img src="{logo}" alt="" />
+            <img className="mx-2 w-10 " src="{logo}" alt="" />
         </div>
 
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
