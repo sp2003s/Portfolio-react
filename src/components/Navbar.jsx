3c8be8457375f2801479ec6817d506a9c680fcda
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-07-25 00:36:00 +0530

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,20 @@
+import logo from "../assets/kevinRushLogo.png";
+import {FaLinkedin} from "react-icons/fa";
+import {FaGithub} from "react-icons/fa";
+import {FaInstagram} from "react-icons/fa";
+
 const Navbar = () => {
-    return <div>Navbar</div>
+    return <nav className = "mb-20 flex items-center justify-between py-6">
+        <div className="flex flex-shrink-0 item-center">
+            <img src="{logo}" alt="" />
+        </div>
+
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+            <FaLinkedin />
+            <FaGithub />
+            <FaInstagram />
+        </div>
+    </nav>
 }
 
 export default Navbar
