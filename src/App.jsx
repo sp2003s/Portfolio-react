0ce035aec9a5b01c90f5338af0a70a05129cd200
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-07-26 01:24:50 +0530

    experience

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from './components/Technologies';
+import Experience from './components/Experience';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Hero />
         <About />
         <Technologies />
+        <Experience />
       </div>
       
     </div>
