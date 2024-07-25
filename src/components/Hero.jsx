5423409eab8b538a728e32b838278a3dc3d076f8
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-07-25 15:18:25 +0530

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,6 @@
+import { HERO_CONTENT } from "../constants";
+import profilePic from "../assets/kevinRushProfile.png";
+
 const Hero = () => {
     return(
         <div className="border-b border-neutral-900 pb-4 lg:mb-35">
@@ -6,14 +9,23 @@ const Hero = () => {
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
                         <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-6xl">Shaurya Pratap Singh</h1>
+                        <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+                            Software Engineer
+                        </span>
+
+                        <p className="my-2 max-w-xl py-6 font-light">
+                            {HERO_CONTENT}
+                        </p>
+                    </div>
+                </div>
+                <div className="w-full lg:w-1/2 lg:p-8">
+                    <div className="flex justify-center">
+                        <img src={profilePic} alt="" />
                     </div>
                 </div>
             </div>
-
         </div>
     );
 };
 
 export default Hero;
-
-//17:17
