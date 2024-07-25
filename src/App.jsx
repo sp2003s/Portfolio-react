2ca296a0c2a66ea9308a49f21f5334af8363c028
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-07-25 18:46:42 +0530

    About me

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <div className='container mx-auto px-8'>
         <Navbar />
         <Hero />
+        <About />
       </div>
       
     </div>
