5d1998d123b2b207b4babefcd9324ff2c377a53e
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-07-26 15:47:18 +0530

    Motion Framer Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,13 @@
 import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/kevinRushProfile.png";
+import { motion } from "framer-motion"
+
+const container = (delay) => ({
+    hidden: {x: -100, opacity: 0},
+    visible: {x: 0, opacity: 1, 
+        transition: {duration: 0.4, delay: delay}
+    },
+})
 
 const Hero = () => {
     return(
@@ -8,19 +16,36 @@ const Hero = () => {
             <div className="flex flex-wrap">
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
-                        <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-6xl">Shaurya Pratap Singh</h1>
-                        <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+                        <motion.h1 
+                        variants={container(0.2)}
+                        initial = "hidden"
+                        animate = "visible"
+                        className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-6xl">Shaurya Pratap Singh</motion.h1>
+                        
+                        <motion.span  
+                        variants={container(0.4)}
+                        initial = "hidden"
+                        animate = "visible"
+                        className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
                             Software Engineer
-                        </span>
+                        </motion.span>
 
-                        <p className="my-2 max-w-xl py-6 font-light">
+                        <motion.p
+                        variants={container(0.6)}
+                        initial = "hidden"
+                        animate = "visible"
+                        className="my-2 max-w-xl py-6 font-light">
                             {HERO_CONTENT}
-                        </p>
+                        </motion.p>
                     </div>
                 </div>
                 <div className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex justify-center">
-                        <img src={profilePic} alt="" />
+                        <motion.img 
+                        initial= {{x: 100, opacity: 0}}
+                        animate= {{x: 0, opacity: 1}}
+                        transition= {{duration:0.5, delay: 0.6}}
+                        src={profilePic} alt="" />
                     </div>
                 </div>
             </div>
