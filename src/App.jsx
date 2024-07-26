e8125c51f616e7e22e2af0fae91025b5e25fe810
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-07-26 15:16:13 +0530

    project section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from './components/Technologies';
 import Experience from './components/Experience';
+import Projects from './components/Projects';
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
         <About />
         <Technologies />
         <Experience />
+        <Projects />
       </div>
       
     </div>
