d25901eba883a5e3bdaa6680db68b5bd3bb39c35
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-07-27 00:05:35 +0530

    framer-motion

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,27 +1,77 @@
 import { RiReactjsLine } from "react-icons/ri"
+import {motion} from "framer-motion";
+
+const iconVariants = (duration) => ({
+    initial: {y: -10},
+    animate: {
+        y: [10, -10],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse",
+        }
+    }
+})
 
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-        <h1 className="my-20 text-center text-4xl">Technologies</h1>
+        <motion.h1 
+        whileInView={{opacity: 1, x: 0}}
+        initial = {{opacity: 0, x: -100}}
+        transition={{duration: 1}}
+        className="my-20 text-center text-4xl">Technologies</motion.h1>
 
-        <div className="flex flex-wrap items-center justify-center gap-4">
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div 
+        whileInView={{opacity: 1, x: 0}}
+        initial = {{opacity: 0, x: -100}}
+        transition={{duration: 1}}
+        className="flex flex-wrap items-center justify-center gap-4">
+            
+            <motion.div 
+            variants={iconVariants(2.5)}
+            initial = "initial"
+            animate = "animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
                 <RiReactjsLine className="text-7xl text-cyan-400" />
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+            variants={iconVariants(3)}
+            initial = "initial"
+            animate = "animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
                 <RiReactjsLine className="text-7xl text-cyan-400" />
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+            variants={iconVariants(2.5)}
+            initial = "initial"
+            animate = "animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
                 <RiReactjsLine className="text-7xl text-cyan-400" />
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+            variants={iconVariants(3)}
+            initial = "initial"
+            animate = "animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
                 <RiReactjsLine className="text-7xl text-cyan-400" />
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+            variants={iconVariants(2.5)}
+            initial = "initial"
+            animate = "animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
                 <RiReactjsLine className="text-7xl text-cyan-400" />
-            </div>
-        </div>
+            </motion.div>
+            <motion.div 
+            variants={iconVariants(3)}
+            initial = "initial"
+            animate = "animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <RiReactjsLine className="text-7xl text-cyan-400" />
+            </motion.div>
+        </motion.div>
     </div>
   )
 }
