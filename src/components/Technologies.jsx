065dd629d978e8c814b21475cb8784c0e65754be
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-07-28 18:45:28 +0530

    techstack icons and framer motion duration

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,79 +1,155 @@
-import { RiReactjsLine } from "react-icons/ri"
-import {motion} from "framer-motion";
+import { RiReactjsLine } from "react-icons/ri";
+import { FaJava, FaPython, FaHtml5, FaCss3Alt, FaNodeJs, FaDatabase, FaGithub, FaGitAlt } from "react-icons/fa";
+import { DiJavascript1 } from "react-icons/di";
+import { SiDjango, SiMongodb, SiPostgresql, SiMysql, SiBootstrap, SiOpencv, SiTensorflow, SiCplusplus } from "react-icons/si";
+import { motion } from "framer-motion";
 
 const iconVariants = (duration) => ({
-    initial: {y: -10},
+    initial: { y: -10 },
     animate: {
         y: [10, -10],
         transition: {
             duration: duration,
-            ease: "linear",
+            ease: "easeInOut",
             repeat: Infinity,
             repeatType: "reverse",
-        }
-    }
-})
+        },
+    },
+});
+
 
 const Technologies = () => {
-  return (
-    <div className="border-b border-neutral-800 pb-24">
-        <motion.h1 
-        whileInView={{opacity: 1, x: 0}}
-        initial = {{opacity: 0, x: -100}}
-        transition={{duration: 1}}
-        className="my-20 text-center text-4xl">Technologies</motion.h1>
+    return (
+        <div className="border-b border-neutral-800 pb-24">
+            <motion.h1
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: -100 }}
+                transition={{ duration: 0.8, ease: "easeInOut" }}
+                className="my-20 text-center text-4xl">
+                Technologies
+            </motion.h1>
 
-        <motion.div 
-        whileInView={{opacity: 1, x: 0}}
-        initial = {{opacity: 0, x: -100}}
-        transition={{duration: 1}}
-        className="flex flex-wrap items-center justify-center gap-4">
-            
-            <motion.div 
-            variants={iconVariants(2.5)}
-            initial = "initial"
-            animate = "animate"
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-                <RiReactjsLine className="text-7xl text-cyan-400" />
-            </motion.div>
-            <motion.div 
-            variants={iconVariants(3)}
-            initial = "initial"
-            animate = "animate"
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-                <RiReactjsLine className="text-7xl text-cyan-400" />
-            </motion.div>
-            <motion.div 
-            variants={iconVariants(2.5)}
-            initial = "initial"
-            animate = "animate"
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-                <RiReactjsLine className="text-7xl text-cyan-400" />
-            </motion.div>
-            <motion.div 
-            variants={iconVariants(3)}
-            initial = "initial"
-            animate = "animate"
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-                <RiReactjsLine className="text-7xl text-cyan-400" />
-            </motion.div>
-            <motion.div 
-            variants={iconVariants(2.5)}
-            initial = "initial"
-            animate = "animate"
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-                <RiReactjsLine className="text-7xl text-cyan-400" />
-            </motion.div>
-            <motion.div 
-            variants={iconVariants(3)}
-            initial = "initial"
-            animate = "animate"
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-                <RiReactjsLine className="text-7xl text-cyan-400" />
-            </motion.div>
-        </motion.div>
-    </div>
-  )
-}
+            <motion.div
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: -100 }}
+                transition={{ duration: 0.8, ease: "easeInOut" }}
+                className="flex flex-wrap items-center justify-center gap-4">
 
-export default Technologies
+                <motion.div
+                    variants={iconVariants(3)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <SiCplusplus className="text-6xl text-cyan-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(4)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <FaJava className="text-6xl text-cyan-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(3)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <FaPython className="text-6xl text-cyan-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(4)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <FaHtml5 className="text-6xl text-cyan-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(3)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <FaCss3Alt className="text-6xl text-cyan-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(4)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <DiJavascript1 className="text-6xl text-cyan-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(3)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <FaNodeJs className="text-6xl text-cyan-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(4)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <SiDjango className="text-6xl text-cyan-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(3)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <RiReactjsLine className="text-7xl text-cyan-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(4)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <SiMongodb className="text-6xl text-cyan-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(3)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <SiMysql className="text-6xl text-cyan-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(4)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <SiBootstrap className="text-6xl text-cyan-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(3)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <FaGithub className="text-6xl text-cyan-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(4)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <FaGitAlt className="text-6xl text-cyan-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(3)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <SiOpencv className="text-6xl text-cyan-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(3)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <SiTensorflow className="text-6xl text-cyan-400" />
+                </motion.div>
+            </motion.div>
+        </div>
+    );
+};
+
+export default Technologies;
