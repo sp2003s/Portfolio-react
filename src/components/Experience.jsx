9615edd93ba36d90caab3a5b1e3c36f9cc3482a5
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-07-28 13:06:11 +0530

    test-justify and about me content

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -30,7 +30,7 @@ const Experience = () => {
                         <h6 className="mb-2 font-semibold">
                             {experience.role} - <span className="text-sm text-purple-100">{experience.company}</span>
                         </h6>
-                        <p className="mb-4 text-neutral-400">{experience.description}</p>
+                        <p className="mb-4 text-neutral-400 text-justify">{experience.description}</p>
                         <div className="flex flex-wrap">
                             {experience.technologies.map((tech, index) => (
                             <span
