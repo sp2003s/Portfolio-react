3809618769f3fea546b9603da1354cbc23339068
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-09-01 01:54:23 +0530

    arrow in resume button

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -34,7 +34,7 @@ const About = () => {
               href="https://drive.google.com/file/d/1AoJz91INm8nAzNo9fdtLissrORkPIXJD/view?usp=sharing" target="_blank"
               className="inline-block px-5 py-2.5 rounded-md text-lg text-center font-bold cursor-pointer select-none outline-none transition-all duration-500 focus:outline-none focus:ring-0 focus:ring-offset-0 hover:-translate-y-1.5 bg-purple-900 text-white w-full sm:w-1/2 lg:w-1/4"
             >
-              My Resume
+              My Resume -{'>'}
             </a>
           </div>
         </motion.div>
